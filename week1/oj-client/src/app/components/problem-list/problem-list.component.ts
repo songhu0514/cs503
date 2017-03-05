a5b9d64239578d2cb14323fc07f6c505bd06153e
SONG HU <songhu0514@gmail.com>
2017-03-04 16:50:31 -0800

    Seperate html & css from ts file

--- week1/oj-client/src/app/components/problem-list/problem-list.component.ts
+++ week1/oj-client/src/app/components/problem-list/problem-list.component.ts
@@ -25,48 +25,8 @@ You may assume that each input would have exactly one solution, and you may not 
 
 @Component({
   selector: 'app-problem-list',
-  template: `
-    <div class="container">
-        <div class="list-group">
-            <a class="list-group-item" *ngFor="let problem of problems">
-                <span class="{{'pull-left label difficulty diff-' + problem.difficulty.toLocaleLowerCase()}}">{{problem.difficulty}}</span>
-                <strong class="title">{{problem.id}}.{{problem.name}}</strong>
-            </a>
-        </div>
-    </div>
-  `,
-  styles: [`
-  .difficulty {
-    min-width: 65px;
-    margin-right: 10px;
-  }   
-  
-  .label.difficulty {
-    padding-top: 0.6em;
-    color: #fbfdfa;
-    font-size: 12px;
-  }
-  
-  .title {
-    font-size: 1.2em;
-  }
-  
-  .diff-easy {
-    background-color: #42ebf4;
-  }
-  
-  .diff-medium {
-      background-color: #92cf5c;
-  }
-  
-  .diff-hard {
-      background-color: #dd0d1e;
-  }
-  
-  .diff-super {
-      background-color: #8d16e2;
-  }
-  `]
+  templateUrl: './problem-list.component.html',
+  styleUrls: ['./problem-list.component.css']
 })
 export class ProblemListComponent implements OnInit {
 
