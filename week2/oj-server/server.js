1d7aa73becfbb10cd81e52a56415877eae5eccc0
SONG HU <songhu0514@gmail.com>
2017-03-12 23:35:13 -0700

    use MongoDB for storage in oj-server

--- week2/oj-server/server.js
+++ week2/oj-server/server.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var app = express()
 var restRouter = require("./routes/rest");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://user:user@ds129030.mlab.com:29030/coj-song");
 
 app.use("/api/v1", restRouter);
 
--- week2/oj-server/services/problemService.js
+++ week2/oj-server/services/problemService.js
@@ -1,60 +1,43 @@
-var problems = [
-  {
-    id: 1,
-    name: "Two Sum",
-    desc: `Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-You may assume that each input would have exactly one solution, and you may not use the same element twice.`,
-    difficulty: "easy"
-  },
-  {
-    id: 2,
-    name: "3Sum",
-    desc: `Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.`,
-    difficulty: "medium"
-  },
-  {
-    id: 3,
-    name: "4Sum",
-    desc: `Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.`,
-    difficulty: "medium"
-  },
-  {
-    id: 4,
-    name: "Sudoku Solver",
-    desc: `Write a program to solve a Sudoku puzzle by filling the empty cells.
-Empty cells are indicated by the character '.'.
-You may assume that there will be only one unique solution.`,
-    difficulty: "hard"
-  },
-  {
-    id: 5,
-    name: "Sliding Window Maximum",
-    desc: `Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.`,
-    difficulty: "super"
-  }
-];
+var ProblemModel = require("../models/problemModel");
 
 var getProblems = function () {
   return new Promise((resolve, reject) => {
-    resolve(problems);
+    ProblemModel.find({}, function(err, problems) {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(problems);
+      }
+    });
   });
 };
 
 var getProblem = function (id) {
   return new Promise((resolve, reject) => {
-    resolve(problems.find(problem => problem.id === id));
+    ProblemModel.findOne({ id : id }, function(err, problems) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(problem);
+        }
+    });
   });
 };
 
 var addProblem = function (newProblem) {
   return new Promise((resolve, reject) => {
-    if(problems.find(problem => problem.name === newProblem.name)) {
-      reject("problem already exists");
-    } else {
-      newProblem.id = problems.length + 1;
-      problems.push(newProblem);
-      resolve(newProblem);
-    }
+    ProblemModel.findOne({ name : newProblem.name }, function(err, problem) {
+        if (problem) {
+          reject("Problem already exists");
+        } else {
+          ProblemModel.count({}, function(err, num) {
+              newProblem.id = num + 1;
+              var mangoProblem = new ProblemModel(newProblem);
+              mangoProblem.save();
+              resolve(newProblem);
+          });
+        }
+    });
   });
 };
 
