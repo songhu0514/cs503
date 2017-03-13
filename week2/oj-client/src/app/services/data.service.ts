f23f9ea99ad82dc3e96325266f52c65d837f6901
SONG HU <songhu0514@gmail.com>
2017-03-13 02:22:25 -0700

    fix url id binding issue

--- week2/oj-client/src/app/services/data.service.ts
+++ week2/oj-client/src/app/services/data.service.ts
@@ -25,7 +25,7 @@ export class DataService {
   }
 
   getProblem(id: number): Promise<Problem> {
-      return this.http.get('api/v1/problem/${id}')
+      return this.http.get(`api/v1/problems/${id}`)
                         .toPromise()
                         .then((res: Response) => res.json())
                         .catch(this.handleError);
