006c88c684d4601ef0bd155851b220c823a408f1
SONG HU <songhu0514@gmail.com>
2017-03-27 22:59:32 -0700

    use node-rest-client to call execution serivce

--- week4/oj-server/routes/rest.js
+++ week4/oj-server/routes/rest.js
@@ -4,6 +4,13 @@ var problemService = require("../services/problemService");
 var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
 
+var node_rest_client = require('node-rest-client').Client;
+var rest_client = new node_rest_client();
+
+EXECUTOR_SERVER_URL = 'http://localhost:5000/build_and_run';
+
+rest_client.registerMethod('build_and_run', EXECUTOR_SERVER_URL, 'POST');
+
 router.get('/problems', function (req, res) {
   problemService.getProblems()
     .then(problems => res.json(problems));
@@ -29,7 +36,19 @@ router.post("/build_and_run", jsonParser, function(req, res) {
   const lang = req.body.lang;
 
   console.log(lang + "; " + userCode);
-  res.json({});
+  rest_client.methods.build_and_run(
+    {
+      data: {code: userCode, lang: lang},
+      header: {'Content-Type': 'application/json'}
+    }, (data,response) => {
+      console.log("Received response from execution server: " + response);
+      const text = `Build output: ${data['build']}
+      Execute output: ${data['run']}`;
+      
+      data['text'] = text;
+      res.json(data);
+    }
+  );
 });
 
 module.exports = router;
