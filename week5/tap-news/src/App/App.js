4ae72094b366447e44a7331bd8feaadfa3a06c6a
SONG HU <songhu0514@gmail.com>
2017-04-05 23:50:39 -0700

    init react project, tap news fontpage

---
+++ week5/tap-news/src/App/App.js
@@ -0,0 +1,20 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+import React from 'react';
+import logo from '../../public/logo.png';
+import './App.css';
+
+class App extends React.Component{
+  render() {
+    return(
+      <div>
+        <img className='logo' src={logo} alt='logo'/>
+        <div className='container'>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
