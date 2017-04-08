c818349bd44d94d37faf5552a4e6479a0dd0208a
SONG HU <songhu0514@gmail.com>
2017-04-08 00:09:02 -0700

    export client code to server

--- week5/web-server/server/routes/index.js
+++ week5/web-server/server/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/*Get home page*/
+router.get('/', function (req, res, next) {
+  //send index.html to start client side
+  res.sendFile("index.html", {root: path.join(__dirname, '../../client/build/')});
 });
 
 module.exports = router;
