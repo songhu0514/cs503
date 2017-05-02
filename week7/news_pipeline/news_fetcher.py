7be88ceaf5b59a5e163baaa79426ace308b8fe29
SONG HU <songhu0514@gmail.com>
2017-05-02 00:16:36 -0700

    use newspaper instead of xpath

--- week7/news_pipeline/news_fetcher.py
+++ week7/news_pipeline/news_fetcher.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+from newspaper import Article
+
 # import common package in parent directory
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'scrapers'))
@@ -26,15 +28,14 @@ def handle_message(msg):
         print 'message is broken'
         return
     task = msg
-    text = None
+    
+    article = Article(task['url'])
+    article.download()
+    article.parse()
 
-    # We support CNN only now
-    if task['source'] == 'cnn':
-        print 'Scraping CNN news'
-        text = cnn_news_scraper.extract_news(task['url'])
-    else:
-        print 'News source [%s] is not supported.' % task['source']
-    task['text'] = text
+    print article.text
+
+    task['text'] = article.text
     dedupe_news_queue_client.sendMessage(task)
  
 while True:
