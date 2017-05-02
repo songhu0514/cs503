0c9252ece4b69710c7895706a80bea451185a29b
SONG HU <songhu0514@gmail.com>
2017-05-02 00:45:02 -0700

    add launcher & multiple news source

--- week7/news_pipeline/news_monitor.py
+++ week7/news_pipeline/news_monitor.py
@@ -20,7 +20,19 @@ SLEEP_TIME_IN_SECOUNDS = 10
 SCRAPE_NEWS_TASK_QUEUE_URL = "amqp://pqfmhgju:8x2vFcULIi6ACRB9od8sasnOKIO3rQXx@donkey.rmq.cloudamqp.com/pqfmhgju"
 SCRAPE_NEWS_TASK_QUEUE_NAME = "tap-news-scrape-news-task-queue"
 
-NEWS_SOURCES = ['cnn']
+NEWS_SOURCES = [
+    'bbc-news',
+    'bbc-sport',
+    'bloomberg',
+    'cnn',
+    'entertainment-weekly',
+    'espn',
+    'ign',
+    'techcrunch',
+    'the-new-york-times',
+    'the-wall-street-journal',
+    'the-washington-post'
+]
 
 redis_client = redis.StrictRedis(REDIS_HOST, REDIS_PORT)
 cloudAMQP_client = CloudAMQPClient(SCRAPE_NEWS_TASK_QUEUE_URL, SCRAPE_NEWS_TASK_QUEUE_NAME)
